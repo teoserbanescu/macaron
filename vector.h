195f954b66f601225eee348ebf98b6a9eee94443
teoserbanescu <teo.teodora16@gmail.com>
2014-09-03 00:54:33 +0300

    Create vector.h

---
+++ vector.h
@@ -0,0 +1,13 @@
+#include <GL/glut.h>
+
+typedef struct vector
+{
+	GLfloat x;
+	GLfloat y;
+	GLfloat z;
+}Vector;
+
+Vector addVectors(Vector a, Vector b);
+Vector substractVectors(Vector a, Vector b);
+Vector normalizeVector(Vector a);
+Vector multiplyVector(Vector a, GLfloat scalar);
